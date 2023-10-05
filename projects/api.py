836ef77d91529757ca53b11dafdb7698d8cbff64
Erick <123erickrh@gmail.com>
2023-10-04 23:09:12 -0500

    Create urls, api and serializers

--- projects/api.py
+++ projects/api.py
@@ -1,6 +1,6 @@
 from .models import Project
 from rest_framework import viewsets, permissions
-from serializers import ProjectSerializer
+from .serializers import ProjectSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
   queryset = Project.objects.all()
